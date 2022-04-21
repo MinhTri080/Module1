532f0dacbcd690eb4e7183c73288817fabe3bf45
LAPTOP-69EUOU7G\Hp <lmtri9118@gmail.com>
2022-04-21 10:01:06 +0700

    vonglapwhile.html

---
+++ vonglapwhile/js.js
@@ -0,0 +1,8 @@
+// let num = prompt('Nhập số từ bàn phím');
+// let total = 0;
+
+// while (num == -1) {
+//     let num = prompt('Nhập số từ bàn phím');
+//     total = total + num;
+//     console.log(total)
+// }
