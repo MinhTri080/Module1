91b410ab12fcaf37ef4f2c3fbda7207656ef7213
LAPTOP-69EUOU7G\Hp <lmtri9118@gmail.com>
2022-04-25 08:43:22 +0700

    vonglap1.html

--- luyentay/luyentay.js
+++ luyentay/luyentay.js
@@ -7,10 +7,10 @@
 //     severNumber = severNumber *10 + surplus;
 // }
 // console.log(`${number} ${number == severNumber ? ' là số đối xứng': 'ko là số đối xứng'}`)
-let soluong = prompt('nhap size');
-let danhsach = new Array(soluong);
-for (let i = 0; i < danhsach.length; i++) {
-    danhsach[i] = prompt('nhap phan tu thu :' + (i + 1));
-    arr[i] = Math.floor(Math.random())
-}
-console.log(danhsach);
+// let soluong = prompt('nhap size');
+// let danhsach = new Array(soluong);
+// for (let i = 0; i < danhsach.length; i++) {
+//     danhsach[i] = prompt('nhap phan tu thu :' + (i + 1));
+//     arr[i] = Math.floor(Math.random())
+// }
+// console.log(danhsach);
